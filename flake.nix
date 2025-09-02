800c1137035117df169cfdcd8d80d01221e2af21
Niklas Tausendpfund <ntausend@proton.me>
2025-09-02 17:49:49 +0200

    gen 18

--- flake.nix
+++ flake.nix
@@ -8,15 +8,30 @@
       url = "github:kamadorueda/alejandra/4.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    home-manager-stable = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    home-manager,
     ...
   } @ inputs: let
     lib = nixpkgs.lib;
   in {
+    #homeConfigurations."emtpy" = home-manager.lib.homeManagerConfiguration
+
     nixosConfigurations = {
       nixi = lib.nixosSystem rec {
         specialArgs = {inherit inputs;};
@@ -25,6 +40,7 @@
 
         modules = [
           ./configuration.nix
+          home-manager.nixosModules.home-manager
           #{
           #environment.systemPackages = [inputs.alejandra.defaultPackage.${system}
           #  nixpkgs.legacyPackages.git];
